dd770ca541b6240e17d7d1763907cdfa55d83bbe
aouledissa <aouledissayousri@gmail.com>
2022-10-26 21:59:06 +0100

    finall version ready for deployment

--- Global/settings.py
+++ Global/settings.py
@@ -39,7 +39,7 @@ ALLOWED_HOSTS = [
 #CORE_URL='http://127.0.0.1:7000/core/'
 
 #production
-CORE_URL='https://e-recyclecoreservice-production.up.railway.app/' 
+CORE_URL='https://e-recyclecoreservice-production.up.railway.app/core' 
 
 
 
